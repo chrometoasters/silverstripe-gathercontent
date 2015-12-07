5d66fabb9d352770cc89c4e7414cce769bb43838
Michal Kleiner <michal.kleiner@chrometoaster.com>
2015-12-07 13:22:44 +1300

    Change SSGatherContentProcessor method's approach so they don't get any params and use func_get_args() instead

--- code/SSGatherContentProcessor.php
+++ code/SSGatherContentProcessor.php
@@ -6,23 +6,34 @@
  * Class dedicated to hold various fields and values processing functions to allow for easy mapping between different
  * conventions used in GatherContent and php code
  *
+ * All function should declare no parameters and access them via func_get_args(). Field or value's content/value
+ * is passed in as the first parameter.
+ *
  */
 class SSGatherContentProcessor extends Object {
 
     /**
      * Remove given prefix from string
      *
-     * @param $field
-     * @param string $prefix
+     * Prefix must start at the very first position of the string.
+     *
+     * Passed in but accessed via func_get_args()
+     * param $value
+     * param string $prefix
+     *
      * @return mixed
      */
-    public function removePrefix($field, $prefix = '') {
+    public static function removePrefix() {
 
-        if (strpos($field, $prefix) === 0) {
-            $field = preg_replace('/' . preg_quote($prefix,'/') . '/', '', $field, 1);
+        $args = func_get_args();
+        $value = $args[0];
+        $prefix = $args[1];
+
+        if (stripos($value, $prefix) === 0) {
+            $value = preg_replace('/' . preg_quote($prefix,'/') . '/i', '', $value, 1);
         }
 
-        return $field;
+        return $value;
     }
 
 
