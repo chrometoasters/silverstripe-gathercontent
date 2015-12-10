aa530660c017c781f767c8a88f8b95f73efd5389
Michal Kleiner <michal.kleiner@chrometoaster.com>
2015-12-11 00:25:55 +1300

    Add simple backup task to download all data and files from GatherContent

---
+++ code/tasks/GatherContentBackupTask.php
@@ -0,0 +1,23 @@
+<?php
+
+class GatherContentBackupTask extends BuildTask {
+
+    protected $title = 'GatherContent backup';
+    protected $description = 'Backup content from GatherContent, cloud based content planning and organising platform (<a href="https://gathercontent.com/">https://gathercontent.com/</a>)';
+    protected $enabled = true;
+
+    private $gc;
+
+    public function __construct() {
+        $this->gc = new SSGatherContent();
+    }
+
+    function run($request) {
+        echo '<br>start time: ' . date('d/m/Y H:i:s') . '<br>' . PHP_EOL;
+
+        $this->gc->backupContentFromGatherContent();
+
+        echo '<br>end time: ' . date('d/m/Y H:i:s') . '<br>' . PHP_EOL;
+    }
+
+}
