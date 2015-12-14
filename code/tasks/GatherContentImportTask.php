b529e935cc50f914b5685bf4dd79d834d6e2e6cb
Michal Kleiner <michal.kleiner@chrometoaster.com>
2015-12-15 05:17:15 +1300

    Add GatherContentImport task (copied from its backup variation)

---
+++ code/tasks/GatherContentImportTask.php
@@ -0,0 +1,23 @@
+<?php
+
+class GatherContentImportTask extends BuildTask {
+
+    protected $title = 'GatherContent import';
+    protected $description = 'Import content from GatherContent, cloud based content planning and organising platform (<a href="https://gathercontent.com/">https://gathercontent.com/</a>)';
+    protected $enabled = true;
+
+    private $gc;
+
+    public function __construct() {
+        $this->gc = new SSGatherContent();
+    }
+
+    function run($request) {
+        echo '<br>start time: ' . date('d/m/Y H:i:s') . '<br>' . PHP_EOL;
+
+        $this->gc->loadContentFromGatherContent();
+
+        echo '<br>end time: ' . date('d/m/Y H:i:s') . '<br>' . PHP_EOL;
+    }
+
+}
